15f407add17bf3320a2ddaf58715068c2e90575e
daffa tabiano <daffatabianoo@gmail.com>
2024-05-27 22:17:27 +0700

    features lock on homescreen and add redux package

--- src/components/fragments/sections/Hero/index.jsx
+++ src/components/fragments/sections/Hero/index.jsx
@@ -1,33 +1,7 @@
-import { useEffect } from 'react';
 import Countdown from '../../Countdown';
 import './Hero.css';
 
 const MainHero = () => {
-    useEffect(() => {
-        const rootElement = document.querySelector(':root');
-        function disableScroll() {
-            scrollTop =
-                window.pageYOffset || document.documentElement.scrollTop;
-            (scrollLeft =
-                window.pageXOffset || document.documentElement.scrollLeft),
-                (window.onscroll = function () {
-                    window.scrollTo(scrollLeft, scrollTop);
-                });
-            rootElement.style.scrollBehavior = 'auto';
-        }
-
-        function enableScroll() {
-            window.onscroll = function () {};
-            rootElement.style.scrollBehavior = 'smooth';
-            localStorage.setItem('open', 'true');
-
-            if (!localStorage.getItem('open')) {
-                disableScroll();
-            }
-        }
-        enableScroll();
-    }, []);
-
     return (
         <section className="hero w-100 p-3 mx-auto text-center d-flex justify-content-center align-items-center text-white">
             <main>
@@ -36,9 +10,9 @@ const MainHero = () => {
                 <p>Akan melangsungkan resepsi pernikahan dalam:</p>
                 <Countdown />
                 <a
+                    id="show-invitation"
                     className="btn btn-lg mt-4"
                     href="#home"
-                    onClick={() => enableScroll()}
                 >
                     Lihat Undangan
                 </a>
--- src/pages/mainpage.jsx
+++ src/pages/mainpage.jsx
@@ -1,6 +1,39 @@
+import { useEffect, useState } from 'react';
 import MainViews from '../components/views/MainViews';
 
 const MainPage = () => {
+    const rootElement = document.querySelector(':root');
+    const home = document.querySelector('#home');
+    const showInvitation = document.querySelector('#show-invitation');
+    const [open, setOpen] = useState(false);
+    const disableScroll = () => {
+        if (window.scrollY >= 0) {
+            rootElement.scrollIntoView({ behavior: 'instant' });
+            setOpen(false);
+            window.scrollTo(0, 0);
+        }
+    };
+
+    const enableScroll = () => {
+        rootElement.scrollIntoView({ behavior: 'smooth' });
+        setOpen(true);
+        window.removeEventListener('scroll', disableScroll);
+        window.scrollTo(0, 200);
+    };
+
+    useEffect(() => {
+        if (!open) {
+            disableScroll();
+            window.addEventListener('scroll', disableScroll);
+        } else {
+            rootElement.scrollIntoView({ behavior: 'smooth' });
+            setOpen(true);
+            window.removeEventListener('scroll', disableScroll);
+            window.scrollTo(0, 200);
+            enableScroll();
+        }
+    }, []);
+
     return <MainViews />;
 };
 
